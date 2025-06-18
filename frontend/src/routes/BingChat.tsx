fe7890e1cf81d987957d39cc08f485390d496d76
yaoxh <yaoxh22@mails.tsinghua.edu.cn>
2025-06-18 15:31:48 +0800

    bing chat

--- frontend/src/routes/BingChat.tsx
+++ frontend/src/routes/BingChat.tsx
@@ -3,6 +3,7 @@ import axios from 'axios';
 import { SessionContext, type SessionInfo } from '../types/SessionContext';
 import { useBehaviorTracker } from '../hooks/useBehaviorTracker';
 import bingLogo from '../assets/bing.png';
+import bingChatLogo from '../assets/bing chat.png';
 import '../styles/BingChat.css';
 
 
@@ -152,11 +153,70 @@ const BingChat: React.FC<{ onExit: (info: SessionInfo) => void }> = ({ onExit })
           </button>
         </div>
       </div>
-      <div className='body'>
-
+      <div className='chat-body'>
+        <div className='chat-content-container' data-component="ChatContent">
+          {messages.map((m, i) => (
+            <div key={i} className='chat-message-container' data-component="ChatMessage">
+              { m.role === 'user' &&
+                <div className="user-message">
+                  {m.content}
+                </div>
+              }
+              { m.role !== 'user' &&
+                <div className="ai-message">
+                  {m.content}
+                </div>
+              }
+            </div>
+          ))}
+        </div>
+        { related.length > 0 && 
+          <div className='chat-related-container' data-component="ChatRelated">
+            {/* <b>相关推荐：</b> */}
+              {related.map((item, idx) => (
+                <div 
+                  key={idx}
+                  onClick={() => {
+                    setInput(item);
+                    handleSend(item);
+                  }}
+                  className='chat-related-box'
+                >
+                  <p className='chat-related-box-title'>{item}</p>
+                  <p className='chat-related-box-url'>https://TODO.com</p>
+                </div>
+                // <button
+                //   key={idx}
+                //   onClick={() => {
+                //     setInput(item);
+                //     handleSend(item);
+                //   }}
+                //   className='related-item'
+                // >
+                //   {item}
+                // </button>
+              ))}
+          </div>
+        }
+        <div className='padding-bottom'></div>
       </div>
-      <div className='footer'>
-        
+      <div className='chat-input-box-container' data-component="ChatInputBox">
+        <img src={bingChatLogo} alt="Bing Chat Logo" className='bing-chat-logo'></img>
+        <input
+          type="text"
+          placeholder='请输入问题...'
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          className='chat-input-box'
+        />
+        <button
+          onClick={() => handleSend()}
+          disabled={!input.trim() || loading}
+          className='chat-send-button'
+          data-component="ChatSendButton"
+        >
+          {loading ? '发送中...' : '发送'}
+        </button>
       </div>
     </div>
   );
